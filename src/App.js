c88039c5b4f6e239d372070b26bdef4bda647fa1
Blake DeGraw <bbdegraw@gmail.com>
2021-05-03 14:33:27 -0700

    created and tested buttons for each wave type

--- src/App.js
+++ src/App.js
@@ -27,16 +27,37 @@ function App() {
     }
   }
 
-  function changeTypeX(){
+  function changeSquare(){
     playX()
     oscX.type = "square"
+    playX()
+  }
+
+  function changeSine(){
+    playX()
+    oscX.type = "sine"
+    playX()
+  }
+
+  function changeSawtooth(){
+    playX()
+    oscX.type = "sawtooth"
+    playX()
+  }
+
+  function changeTriangle(){
+    playX()
+    oscX.type = "triangle"
     playX()
   }
 
   return (
     <div className="App">
       <button onClick={playX}>play</button>
-      <button onClick={changeTypeX}>square</button>
+      <button onClick={changeSquare}>square</button>
+      <button onClick={changeSine}>sine</button>
+      <button onClick={changeSawtooth}>sawtooth</button>
+      <button onClick={changeTriangle}>triangle</button>
     </div>
   )
 }
