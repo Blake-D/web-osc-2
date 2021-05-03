1722a440245957efa8237baa7a54f2e35085677f
Blake DeGraw <bbdegraw@gmail.com>
2021-05-03 13:52:26 -0700

    play button working. starts and stops

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import './App.css'
 
 function App() {
+
+  var oscillatorX
+
+  let oscX = {
+    type: "sine",
+    frequency: 200,
+    playing: false
+  }
+
+  var audioContext = new AudioContext()
+
+  function playX(){
+    if(oscX.playing){
+      oscillatorX.stop()
+      oscX.playing = false
+    } else{
+      oscillatorX = audioContext.createOscillator()
+      oscillatorX.type = oscX.type
+      oscillatorX.frequency.setValueAtTime(oscX.frequency, audioContext.currentTime)
+      oscillatorX.connect(audioContext.destination)
+      oscillatorX.start()
+      oscX.playing = true
+    }
+  }
+
   return (
     <div className="App">
-      <h1>sanity</h1>
+      <button onClick={playX}>play</button>
     </div>
   )
 }
