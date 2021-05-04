65ad163a3b9dc65543be33b196344da44e77478e
Blake DeGraw <bbdegraw@gmail.com>
2021-05-04 16:11:55 -0700

    oscX is working pretty much as it should. calling it a day

--- src/App.js
+++ src/App.js
@@ -3,56 +3,11 @@ import React, { useState, useEffect } from 'react'
 
 function App() {
 
-  // const useMousePosition = () => {
-  //   const [mousePosition, setMousePosition] = useState({ x: null, y: null });
-
-  //   const updateMousePosition = e => {
-  //     setMousePosition({ x: e.clientX, y: e.clientY });
-  //   };
-
-  //   useEffect(() => {
-  //     window.addEventListener("mousemove", updateMousePosition);
-
-  //     return () => window.removeEventListener("mousemove", updateMousePosition);
-  //   }, []);
-
-  //   return mousePosition;
-  // };
-
-  // const {x,y} = useMousePosition()
-
-  // var oscillatorX
-
-  // let oscX = {
-  //   type: "sine",
-  //   frequency: x,
-  //   playing: false
-  // }
-
-  // const audioContext = new AudioContext()
-
-  // function PlayX(){
-  //   if(oscX.playing){
-  //     oscillatorX.stop()
-  //     oscX.playing = false
-  //   } else{
-  //     oscillatorX = audioContext.createOscillator()
-  //     oscillatorX.type = oscX.type
-  //     oscillatorX.frequency.setValueAtTime(oscX.frequency, audioContext.currentTime)
-  //     oscillatorX.connect(audioContext.destination)
-  //     oscillatorX.start()
-  //     oscX.playing = true
-  //   }
-  // }
-
-  // window.addEventListener("mousedown", PlayX)
-  // window.addEventListener("mouseup", PlayX)
-
   var playX,
       oscillatorX
 
   let oscX = { // primary oscillator for all possible combos, frequency is tied to X-axis
-    type: "sine",
+    type: "sawtooth",
     frequency: 200,
     playing: false
   }
@@ -87,18 +42,16 @@ function App() {
     }
 
     // determines the state of the mouse (down or up). the mousemove event listener below can only call the changeFreq functions if mouseState is true
-    let mouseState = false
+    // let mouseState = false
     let x = null
     let y = null
 
     document.getElementById("test-area").addEventListener('mousedown', () => {
-      console.log('firing!')
-      mouseState = true
       playX()
     })
 
     document.getElementById("test-area").addEventListener('mouseup', () => {
-      mouseState = false
+      playX()
     })
 
     function changeFreqX() {
@@ -110,7 +63,8 @@ function App() {
       x = e.clientX
       y = e.clientY
       changeFreqX()
-      console.log(x)
+      playX()
+      playX()
     })
 
     // document.getElementById("test-area").addEventListener('mousemove', () => { // calls the changeFreq functions if mouse is down
@@ -125,7 +79,6 @@ function App() {
   return (
     <div className="App">
       <div id="test-area"></div>
-      {/* <p>X is: {x}</p> */}
     </div>
   )
 }
